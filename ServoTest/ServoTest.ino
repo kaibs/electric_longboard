2aa7c39df358baea2805dc03122c17b04ce40738
kaibs <kai.b.schneider@gmail.com>
2020-03-14 23:50:08 +0100

    fourth release

--- ServoTest/ServoTest.ino
+++ ServoTest/ServoTest.ino
@@ -1,20 +1,29 @@
+/*
+Coded by Marjan Olesch
+Sketch from Insctructables.com
+Open source - do what you want with this code!
+*/
+#include <Servo.h>
 
-#include  <Servo.h>
 
-Servo esc;
-int potpin = 0;
-int val=0;
-void setup()
-{
-esc.attach(9);
-Serial.begin(9600);
-esc.write(40); 
+int value = 0; // set values you need to zero
+
+Servo firstESC, secondESC; //Create as much as Servoobject you want. You can controll 2 or more Servos at the same time
+
+void setup() {
+
+  firstESC.attach(9);    // attached to pin 9 I just do this with 1 Servo
+  Serial.begin(9600);    // start serial at 9600 baud
+
 }
-void loop()
-{
-val = analogRead(potpin);
-//val = map(val, 0, 1023, 0, 180);
-val= map(val, 0, 1023,1250,2500);
-Serial.println(val);
-esc.writeMicroseconds(val);
+
+void loop() {
+
+//First connect your ESC WITHOUT Arming. Then Open Serial and follo Instructions
+ 
+  firstESC.writeMicroseconds(value);
+ 
+  if(Serial.available()) 
+    value = Serial.parseInt();    // Parse an Integer from Serial
+
 }
